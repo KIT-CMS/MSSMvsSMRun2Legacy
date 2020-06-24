bd825dbbc61a87288b5158bbabbf94fc21aa581e
Artur Gottmann <artur.gottmann@kit.edu>
2020-06-24 19:03:02 +0200

    scripts: extend scripts for parallel prefit shape creation (continued)

--- scripts/prefitshapes_parallel.py
+++ scripts/prefitshapes_parallel.py
@@ -13,13 +13,15 @@ def execute(cmd):
 
 
 datacard_pattern = sys.argv[1]
+freezeargs = sys.argv[2]
+wsname = sys.argv[3]
 ncores = 10
-if len(sys.argv) > 2:
-    ncores = int(sys.argv[2])
+if len(sys.argv) > 4:
+    ncores = int(sys.argv[4])
 
 datacards = [d for d in glob.glob(datacard_pattern) if "/cmb/" not in d]
 
-cmds = ['card=DATACARD; echo $card; echo ${card/combined.txt.cmb/stage0.root}; echo ${card/combined.txt.cmb/prefit_shapes.root}; PostFitShapesFromWorkspace -w ${card/combined.txt.cmb/stage0.root}  -o ${card/combined.txt.cmb/prefit_shapes.root} -d ${card}'.replace("DATACARD",d) for d in datacards]
+cmds = ['card=DATACARD; basedir=$(dirname $(dirname ${card})); category=$(basename $(dirname ${card})); echo $card; echo ${card/combined.txt.cmb/WSNAME}; echo ${card/combined.txt.cmb/prefit_shapes.root}; PostFitShapesFromWorkspace -w ${card/combined.txt.cmb/WSNAME}  -o ${card/combined.txt.cmb/prefit_shapes.root} -d ${basedir}/restore_binning/${category}/${category}.txt FREEZEARGS'.replace("DATACARD",d).replace("FREEZEARGS",freezeargs).replace("WSNAME",wsname) for d in datacards]
 
 p = Pool(ncores)
 p.map(execute, cmds)
