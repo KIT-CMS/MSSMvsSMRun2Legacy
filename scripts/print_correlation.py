f6c76f366cd1be15e6840240cbc1d819ccf949ef
Artur Gottmann <artur.gottmann@kit.edu>
2020-08-06 10:00:57 +0200

    scripts/print_correlation.py: add short script to print correlation of 2 parameters

---
+++ scripts/print_correlation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import ROOT as R
+R.gROOT.SetBatch()
+import sys
+
+fpath = sys.argv[1]
+poi1 = sys.argv[2]
+poi2 = sys.argv[3]
+
+F = R.TFile.Open(fpath,"read")
+result = F.Get("fit_s")
+params = result.floatParsInit()
+correlation =  round(result.correlation(params.find(poi1),params.find(poi2)),2)
+print "Determined correlation between {POI1} and {POI2}: {CORR}".format(POI1=poi1, POI2=poi2, CORR=correlation)
