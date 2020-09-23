9e0ded764b09355a6ac0881959238f11ba65c698
Artur Gottmann <artur.gottmann@kit.edu>
2020-09-23 14:33:57 +0200

    scripts/prefit_postfit_shapes_parallel.py: allow to specify output name

--- scripts/prefit_postfit_shapes_parallel.py
+++ scripts/prefit_postfit_shapes_parallel.py
@@ -15,6 +15,7 @@ def execute(cmd):
 parser = argparse.ArgumentParser( description = "Script to run 'PostFitShapesFromWorkspace' in parallel for each category.")
 parser.add_argument('--datacard_pattern', required = True, help = "Path pattern to the 'combined.txt.cmb' datacards to be used for the histograms")
 parser.add_argument('--workspace_name', required = True, help = "Workspace name to be used to derive systematic uncertainties")
+parser.add_argument('--output_name', default="prefit_shapes.root", help = "Name of the output files")
 parser.add_argument('--freeze_arguments', default="", help = "Arguments to be frozen to a certain value. Use with options, e.g. '--freeze r=1'")
 parser.add_argument('--fit_arguments', default="",
                     help = "Arguments to be used to apply fit result(s). Use with needed options, e.g. '-f <path-pattern-to-fitDiagnostics.root>:<fit-to-be-used> --sampling --postfit'")
@@ -25,7 +26,7 @@ args = parser.parse_args()
 
 datacards = [d for d in glob.glob(args.datacard_pattern) if "/cmb/" not in d]
 
-cmds = ['card=DATACARD; basedir=$(dirname $(dirname ${card})); category=$(basename $(dirname ${card})); echo $card; echo ${card/combined.txt.cmb/WSNAME}; echo ${card/combined.txt.cmb/prefit_shapes.root}; PostFitShapesFromWorkspace -w ${card/combined.txt.cmb/WSNAME}  -o ${card/combined.txt.cmb/prefit_shapes.root} -d ${basedir}/restore_binning/${category}/${category}.txt FREEZEARGS FITARGS'.replace("DATACARD",d).replace("FREEZEARGS",args.freeze_arguments).replace("FITARGS",args.fit_arguments).replace("WSNAME",args.workspace_name) for d in datacards]
+cmds = ['card=DATACARD; basedir=$(dirname $(dirname ${card})); category=$(basename $(dirname ${card})); echo $card; echo ${card/combined.txt.cmb/WSNAME}; echo ${card/combined.txt.cmb/OUTPUT}; PostFitShapesFromWorkspace -w ${card/combined.txt.cmb/WSNAME}  -o ${card/combined.txt.cmb/OUTPUT} -d ${basedir}/restore_binning/${category}/${category}.txt FREEZEARGS FITARGS'.replace("DATACARD",d).replace("FREEZEARGS",args.freeze_arguments).replace("FITARGS",args.fit_arguments).replace("WSNAME",args.workspace_name).replace("OUTPUT",args.output_name) for d in datacards]
 
 p = Pool(args.parallel)
 if args.dry_run:
