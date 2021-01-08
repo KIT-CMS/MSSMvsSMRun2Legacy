580f8e75aad75af04170277d38ab10f0d21402d8
Artur Gottmann <artur.gottmann@kit.edu>
2021-01-08 17:30:38 +0100

    checkout.sh: updates of CMSSW, and MSSM scenarios

--- scripts/checkout.sh
+++ scripts/checkout.sh
@@ -2,7 +2,7 @@
 
 ### Setup of CMSSW release
 NUM_CORES=10
-CMSSW=CMSSW_10_2_21
+CMSSW=CMSSW_10_2_25
 
 export SCRAM_ARCH=slc7_amd64_gcc700
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
@@ -29,25 +29,29 @@ git clone git@github.com:KIT-CMS/MSSMvsSMRun2Legacy.git CombineHarvester/MSSMvsS
 # SM analysis specific code
 git clone git@github.com:KIT-CMS/SMRun2Legacy.git CombineHarvester/SMRun2Legacy
 
-# Install LHCHXSWGMSSMNeutral packages
-wget -O CombineHarvester/MSSMvsSMRun2Legacy/python/mssm_xs_tools.py https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHXSWGMSSMNeutral/mssm_xs_tools.py_v2.2
+# Install LHCHWGMSSMNeutral packages
+wget -O CombineHarvester/MSSMvsSMRun2Legacy/python/mssm_xs_tools.py https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mssm_xs_tools.py_v2.4
 sed -i "s!./mssm_xs_tools_C.so!${CMSSW_BASE}/lib/${SCRAM_ARCH}/libCombineHarvesterMSSMvsSMRun2Legacy.so!g" CombineHarvester/MSSMvsSMRun2Legacy/python/mssm_xs_tools.py
 sed -i "s!root_files/mh125_13.root!${CMSSW_BASE}/src/CombineHarvester/MSSMvsSMRun2Legacy/data/mh125_13.root!g" CombineHarvester/MSSMvsSMRun2Legacy/python/mssm_xs_tools.py
-wget -O CombineHarvester/MSSMvsSMRun2Legacy/src/mssm_xs_tools.cc https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHXSWGMSSMNeutral/mssm_xs_tools.C_v2.3
+wget -O CombineHarvester/MSSMvsSMRun2Legacy/src/mssm_xs_tools.cc https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mssm_xs_tools.C_v2.4
 sed -i 's!mssm_xs_tools.h!CombineHarvester/MSSMvsSMRun2Legacy/interface/mssm_xs_tools.h!g' CombineHarvester/MSSMvsSMRun2Legacy/src/mssm_xs_tools.cc
-wget -O CombineHarvester/MSSMvsSMRun2Legacy/interface/mssm_xs_tools.h https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHXSWGMSSMNeutral/mssm_xs_tools.h_v2.3
+wget -O CombineHarvester/MSSMvsSMRun2Legacy/interface/mssm_xs_tools.h https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mssm_xs_tools.h_v2.4
 
 # Compile everything
 scramv1 b clean; scramv1 b -j $NUM_CORES
 
 # Download root files for latest MSSM benchmark scenarios
-wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHXSWGMSSMNeutral/mh125_13.root
-wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHXSWGMSSMNeutral/mh125_ls_13.root
-wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHXSWGMSSMNeutral/mh125_lc_13.root
-wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHXSWGMSSMNeutral/mh125_align_13.root
-wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHXSWGMSSMNeutral/mh1125_CPV_13.root
-wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHXSWGMSSMNeutral/mh125EFT_13.root
-wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHXSWGMSSMNeutral/mh125EFT_lc_13.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh125_13.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh125_ls_13.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh125_lc_13.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh125_align_13.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mHH125_13.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh1125_CPV_13.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh125_muneg_1_13.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh125_muneg_2_13.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh125_muneg_3_13.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh125EFT_13.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh125EFT_lc_13.root
 
 # Download ggH NLO reweighting inputs
 git archive --remote=ssh://git@gitlab.cern.ch:7999/cms-htt/MSSM-Full-2016.git HEAD Models/higgs_pt_v3.root | tar -C CombineHarvester/MSSMvsSMRun2Legacy/data -xf - Models/higgs_pt_v3.root --strip-components=1
