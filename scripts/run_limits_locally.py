d4b095bcc20f66f2db6f30b54fbf1689fdc62717
Sebastian Brommer <sebastian.brommer@kit.edu>
2021-02-02 12:35:35 +0100

    add additional option to local running of fits

--- scripts/run_limits_locally.py
+++ scripts/run_limits_locally.py
@@ -10,9 +10,10 @@ parser.add_argument("--taskname",
                     required=True,
                     help="name of the condor task script")
 parser.add_argument('--cores', default=20, help="number of cores to be used")
+parser.add_argument('--njobs', default=2820, help="number of jobs that are processed")
 
 args = parser.parse_args()
-index_list = range(0, 2820)
+index_list = range(0, int(args.njobs))
 
 
 def command(index):
