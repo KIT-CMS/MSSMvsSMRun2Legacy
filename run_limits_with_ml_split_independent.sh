ecab7d16ae6698317dd3f014e4824c7670d02eb1
Sebastian Brommer <sebastian.brommer@kit.edu>
2021-02-05 14:16:58 +0100

    adapt ind limits script

--- run_limits_with_ml_split_independent.sh
+++ run_limits_with_ml_split_independent.sh
@@ -39,7 +39,7 @@ if [[ $MODE == "initial" ]]; then
         --variable nnscore \
         --sm_gg_fractions ${CMSSW_BASE}/src/CombineHarvester/MSSMvsSMRun2Legacy/data/higgs_pt_v3.root \
         --sm \
-        --parallel 1 2>&1 | tee -a ${defaultdir}/logs/morph_sm_log.txt
+        --parallel 6 2>&1 | tee -a ${defaultdir}/logs/morph_sm_log.txt
 
     morph_parallel.py --output ${defaultdir}/datacards \
         --analysis ${analysis} \
@@ -47,7 +47,7 @@ if [[ $MODE == "initial" ]]; then
         --category_list ${CMSSW_BASE}/src/CombineHarvester/MSSMvsSMRun2Legacy/input/by_channel/mssm_signal_categories_$CHANNEL.txt \
         --variable mt_tot_puppi \
         --sm_gg_fractions ${CMSSW_BASE}/src/CombineHarvester/MSSMvsSMRun2Legacy/data/higgs_pt_v3.root \
-        --parallel 1 2>&1 | tee -a ${defaultdir}/logs/morph_mssm_log.txt
+        --parallel 6 2>&1 | tee -a ${defaultdir}/logs/morph_mssm_log.txt
 
     ############
     # combining outputs
@@ -74,6 +74,16 @@ elif [[ $MODE == "ws" ]]; then
     -i ${datacarddir}/${ERA}/${CHANNEL} \
     -m 110 --parallel 4 | tee -a ${defaultdir}/logs/workspace_independent_${ERA}_${CHANNEL}.txt
 
+
+    # # Extract prefit shapes.
+    # prefit_postfit_shapes_parallel.py --datacard_pattern "output_mssm/201?/htt_*/combined.txt.cmb" \
+    #                                 --workspace_name ws.root \
+    #                                 --freeze_arguments "--freeze $FREEZE" \
+    #                                 --output_name prefit_shapes_${FREEZE}.root
+    #                                 --parallel $CORES
+
+    # hadd -f prefit_shapes_${FREEZE}.root output_mssm/201?/htt_*/prefit_shapes_${FREEZE}.root
+
     ############
     # job setup creation
     ############
@@ -94,6 +104,7 @@ elif [[ $MODE == "ws" ]]; then
     --cminDefaultMinimizerStrategy 0 \
     --cminDefaultMinimizerTolerance 0.01 \
     -v 1 | tee -a ${defaultdir}/logs/job_setup_modelind_bbh_${ERA}_${CHANNEL}.txt
+
 
     combineTool.py -m "110,120,130,140,160,180,200,250,300,350,400,450,500,600,700,800,900,1000,1200,1400,1600,1800,2000,2300,2600,2900,3200" \
     -M AsymptoticLimits \
@@ -126,24 +137,61 @@ elif [[ $MODE == "submit" ]]; then
     ############
     cp scripts/run_limits_locally.py ${defaultdir}/limits_ind/condor
     cd ${defaultdir}/limits_ind/condor
-    python run_limits_locally.py --cores 10 --njobs 27 --taskname condor_bbH_full_${ERA}_${CHANNEL}.sh
-    python run_limits_locally.py --cores 10 --njobs 27 --taskname condor_ggH_full_${ERA}_${CHANNEL}.sh
+    python run_limits_locally.py --cores 27 --njobs 27 --taskname condor_bbH_full_${ERA}_${CHANNEL}.sh | tee -a ${defaultdir}/logs/fit_bbh_${ERA}_${CHANNEL}.txt
+    python run_limits_locally.py --cores 27 --njobs 27 --taskname condor_ggH_full_${ERA}_${CHANNEL}.sh | tee -a ${defaultdir}/logs/fit_ggh_${ERA}_${CHANNEL}.txt
 
 elif [[ $MODE == "collect" ]]; then
     for p in gg bb
     do
         combineTool.py -M CollectLimits ${datacarddir}/${ERA}/${CHANNEL}/higgsCombine.${p}H*.root \
-        --use-dirs \
-        -o ${datacarddir}/${ERA}/${CHANNEL}/mssm_${p}H_${ERA}_${CHANNEL}.json
-
-        plotMSSMLimits.py --cms-sub "Own Work" \
-        --title-right="${CHANNEL} - ${LUMI}" \
-        --process "${p}#phi" \
-        --y-axis-min 0.0001 \
-        --y-axis-max 1000.0 \
-        --show exp,obs ${datacarddir}/${ERA}/${CHANNEL}/mssm_${p}H_${ERA}_${CHANNEL}_${CHANNEL}.json  \
-        --output mssm_model-independent_${p}H_${ERA}_${CHANNEL} \
-        --logx \
-        --logy
+            --use-dirs \
+            -o ${datacarddir}/${ERA}/${CHANNEL}/mssm_${p}H_${ERA}_${CHANNEL}.json
     done
+
+elif [[ $MODE == "plot" ]]; then
+    for p in gg bb
+    do
+        plotMSSMLimits.py --cms-sub "Own Work" \
+            --title-right="${CHANNEL} - ${LUMI}" \
+            --process "${p}#phi" \
+            --y-axis-min 0.0001 \
+            --y-axis-max 1000.0 \
+            --show exp,obs ${datacarddir}/${ERA}/${CHANNEL}/mssm_${p}H_${ERA}_${CHANNEL}_${CHANNEL}.json  \
+            --output mssm_model-independent_${p}H_${ERA}_${CHANNEL} \
+            --logx \
+            --logy
+        mv mssm_model-independent_${p}H_${ERA}_${CHANNEL}.{png,pdf} ${defaultdir}/limits_ind
+    done
+
+elif [[ $MODE == "prefit" ]]; then
+    FREEZE="MH=450,r_ggH=0.002,r_bbH=0.002"
+    prefit_postfit_shapes_parallel.py --datacard_pattern "analysis/mt_16_debug_independent/datacards_mssm/$ERA/$CHANNEL/combined.txt.cmb" \
+                                  --workspace_name ws.root \
+                                  --freeze_arguments "--freeze $FREEZE" \
+                                  --output_name prefit_shapes_${FREEZE}.root \
+                                  --parallel 1
+
+    hadd -f prefit_shapes_${FREEZE}.root output_mssm/201?/htt_*/prefit_shapes_${FREEZE}.root
+
+elif [[ $MODE == "impacts" ]]; then
+    combineTool.py -M Impacts -d ${datacarddir}/${ERA}/${CHANNEL}/ws.root \
+        -m 125 \
+        --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy 0 \
+        --doInitialFit --robustFit 1 \
+        -t -1 --setParameters r_ggH=0,r_bbH=0 \
+        --parallel 16
+
+    combineTool.py -M Impacts -d ${datacarddir}/${ERA}/${CHANNEL}/ws.root \
+        -m 125 \
+        --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy 0 \
+        --robustFit 1 --doFits \
+        -t -1 --setParameters r_ggH=0,r_bbH=0 \
+        --parallel 16
+
+    combineTool.py -M Impacts \
+        -d ${datacarddir}/${ERA}/${CHANNEL}/ws.root \
+        -m 125 \
+        -o ${ERA}_${CHANNEL}_impacts.json
+
+    plotImpacts.py -i ${ERA}_${CHANNEL}_impacts.json -o ${ERA}_${CHANNEL}_impacts
 fi
