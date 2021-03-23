64146d16e4f0cd8158f0a9f50aebb1fcd3eee96d
Maximilian Burkart <maxiburkart@gmail.com>
2021-03-23 13:48:06 +0100

    Add first version of code to plot model independent limits.

---
+++ plotting/plot_shapes_mssm_model_independent.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source utils/setup_cvmfs_sft.sh
+source utils/setup_python.sh
+
+ERA=$1
+INPUT=$2
+OUTPUT=$3  # Output directory the plots are written to.
+IFS="," read -a CHANNELS <<< $4
+MASS=$5
+
+[[ -z $MASS ]] && MASS=1200
+
+
+if [[ ! -d "$OUTPUT" ]]
+then
+    mkdir $OUTPUT
+fi
+
+for FILE in $INPUT
+do
+    for OPTION in "" "--png"
+    do
+        ./plotting/plot_shapes_mssm.py -i $FILE \
+                                       -c ${CHANNELS[@]} \
+                                       -e $ERA \
+                                       $OPTION \
+                                       --fake-factor \
+                                       --embedding \
+                                       --normalize-by-bin-width \
+                                       -o $OUTPUT \
+                                       --model-independent \
+                                       --blinded \
+                                       --mass $MASS
+    done
+done
