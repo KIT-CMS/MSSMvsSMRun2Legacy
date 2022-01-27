5aecd9b832dafcc202f4c7eb18d7a438f2aef348
Maximilian Burkart <maxiburkart@gmail.com>
2022-01-27 17:13:52 +0100

    Adapt plot range for additonal mode of model-dep limits.

--- run_model-dep-additional_limits.sh
+++ run_model-dep-additional_limits.sh
@@ -67,8 +67,8 @@ elif [[ $MODEL == "mh125_align" ]]; then
     sm_like_mass="m_h"
     x_title='m_{A} [GeV]'
     mass_histogram_title="m_{h}"
-    y_min=1.0
-    y_max=20.0
+    y_min=3.0
+    y_max=12.0
 elif [[ $MODEL == "mHH125" ]]; then
     wsoutput="ws_mHH125.root"
     modelfile="13,Run2017,mHH125_13.root"
@@ -98,7 +98,7 @@ elif [[ $MODEL == "mh125_muneg_1" ]]; then
     sm_like_mass="m_h"
     x_title='m_{A} [GeV]'
     mass_histogram_title="m_{h}"
-    y_min=1.0
+    y_min=4.0
     y_max=56.0
 elif [[ $MODEL == "mh125_muneg_2" ]]; then
     wsoutput="mh125_muneg_2.root"
@@ -108,7 +108,7 @@ elif [[ $MODEL == "mh125_muneg_2" ]]; then
     sm_like_mass="m_h"
     x_title='m_{A} [GeV]'
     mass_histogram_title="m_{h}"
-    y_min=1.0
+    y_min=5.0
     y_max=30.0
 elif [[ $MODEL == "mh125_muneg_3" ]]; then
     wsoutput="mh125_muneg_3.root"
@@ -118,7 +118,7 @@ elif [[ $MODEL == "mh125_muneg_3" ]]; then
     sm_like_mass="m_h"
     x_title='m_{A} [GeV]'
     mass_histogram_title="m_{h}"
-    y_min=1.0
+    y_min=6.0
     y_max=20.0
 ### EFT scenarios #####
 elif [[ $MODEL == "mh125EFT" ]]; then
@@ -261,6 +261,8 @@ elif [[ $MODE == "ws" ]]; then
         -i ${datacarddir}/combined/cmb/ 2>&1 | tee -a ${defaultdir}/logs/workspace_${MODEL}.txt
     fi
 
+elif [[ $MODE == "setup" ]]; then
+
     ############
     # job setup creation
     ############
@@ -350,7 +352,6 @@ elif [[ $MODE == "copy-results-gc" ]]; then
     # job submission
     ############
     rsync -avhP /storage/gridka-nrg/${GRIDUSER}/gc_storage/combine/${taskname}/output/ ${defaultdir}/limits_${MODEL}/condor
-
 
 elif [[ $MODE == "collect" ]]; then
     ############
