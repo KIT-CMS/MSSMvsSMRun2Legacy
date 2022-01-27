406db53b4a7f126b58e2a6689e319f142fcfde11
Maximilian Burkart <maxiburkart@gmail.com>
2022-01-27 11:28:31 +0100

    Add scipt to download old root files

---
+++ scripts/download_old_model_files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Download model root files from the twiki page.
+# These are not updated at the moment and are thus available in
+# the older version.
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh125_13.root -O mh125_13_old.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh125_ls_13.root -O mh125_ls_13_old.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh125_lc_13.root -O mh125_lc_13_old.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh125_align_13.root -O mh125_align_13_old.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mHH125_13.root -O mHH125_13_old.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh1125_CPV_13.root -O mh1125_CPV_13_old.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh125_muneg_1_13.root -O mh125_muneg_1_13_old.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh125_muneg_2_13.root -O mh125_muneg_2_13_old.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh125_muneg_3_13.root -O mh125_muneg_3_13_old.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh125EFT_13.root -O mh125EFT_13_old.root
+wget -P CombineHarvester/MSSMvsSMRun2Legacy/data https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHWGMSSMNeutral/mh125EFT_lc_13.root -O mh125EFT_lc_13_old.root
