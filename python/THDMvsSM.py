fd8530bdcdfe31abaf4dda665e09156de35f90ec
Maximilian Burkart <maxiburkart@gmail.com>
2023-03-13 17:43:49 +0100

    Add final version of 2HDM limit calculation

--- python/THDMvsSM.py
+++ python/THDMvsSM.py
@@ -2,6 +2,7 @@
 import os
 from collections import defaultdict
 import math
+import json
 
 import numpy as np
 
@@ -13,17 +14,41 @@ from HiggsAnalysis.CombinedLimit.PhysicsModel import *
 class THDMvsSMHiggsModel(PhysicsModel):
 
     _binning = {
-            "BP1_Type2": {
-                "mH": np.arange(125., 1075., 50.),
+            "BP1_Type1": {
+                "mH": np.arange(137.5, 612.5, 25.),
                 "tanb": np.arange(1.5, 51.5, 1.),
                 },
-            "BP1_Type1": {
-                "mH": np.arange(125., 1075., 50.),
-                "tanb": np.arange(1.5, 51.5, 1.),
+            "BP1_Type1_mod": {
+                "mH": np.arange(145., 615., 10.),
+                "tanb": np.arange(1.05, 10.25, 0.1),
+                },
+            "BP1_Type2": {
+                "mH": np.arange(145., 610., 10.),
+                "tanb": np.arange(1.0, 20.4, 0.2),
+                },
+            "FixedMass_Type1": {
+                "cos_betal": np.arange(-0.51, 0.53, 0.02),
+                "tanb": np.arange(1., 51., 2.),
                 },
             "FixedMass_Type2": {
                 "cos_betal": np.arange(-0.51, 0.53, 0.02),
                 "tanb": np.arange(1., 51., 2.),
+                },
+            "HWWLike_Type1": {
+                "mH": np.arange(135., 515., 10.),
+                "tanb": np.arange(0.45, 10.25, 0.1),
+                },
+            "HWWLike_Type2": {
+                "mH": np.arange(135., 510., 5.),
+                "tanb": np.arange(0.45, 10.05, 0.1),
+                },
+            "PASComp_Type1": {
+                "mH": np.arange(135., 515., 10.),
+                "tanb": np.arange(0.45, 10.15, 0.1),
+                },
+            "PASComp_Type2": {
+                "mH": np.arange(137.5, 502.5, 5.),
+                "tanb": np.arange(0.45, 10.05, 0.1),
                 },
     }
 
@@ -38,6 +63,9 @@ class THDMvsSMHiggsModel(PhysicsModel):
         self.filename = ''
         self.debug_output = None
         self.x_variable = "mH"
+        self.use_hSM_difference = False
+        self.sm_predictions = None
+        self.replace_with_sm125 = True
 
     def setPhysicsOptions(self,physOptions):
         for po in physOptions:
@@ -54,6 +82,17 @@ class THDMvsSMHiggsModel(PhysicsModel):
                 debug_name = po.replace('debug-output=', '')
                 self.debug_output = ROOT.TFile.Open(debug_name, "recreate")
                 print "Using %s as debug output file"%debug_name
+            if po.startswith('hSM-treatment='):
+                hSM_treatment = po.replace('hSM-treatment=', '')
+                self.use_hSM_difference = hSM_treatment == "hSM-in-bg"
+                print "Using (BSM - SM) difference for SM-like Higgs boson?",self.use_hSM_difference
+            if po.startswith('sm-predictions='):
+                sm_pred_path = po.replace('sm-predictions=','')
+                self.sm_predictions = json.load(open(sm_pred_path,'r'))
+                print "Using %s for SM predictions"%sm_pred_path
+            if po.startswith('replace-with-SM125='):
+                self.replace_with_sm125 = bool(int(po.replace('replace-with-SM125=', ''))) # use either 1 or 0 for the choice
+                print "Replacing with SM 125?",self.replace_with_sm125
         self.filename = os.path.join(filePrefix, modelFile)
         if "FixedMass" in modelFile.replace(".root", ""):
             self.x_variable = "cos_betal"
@@ -75,42 +114,52 @@ class THDMvsSMHiggsModel(PhysicsModel):
 
         procs = ["h", "H", "A"]
 
+        # If we want to replace the SM like Higgs boson with SM templates,
+        # we do have the split in different loop contributions.
+        # For bbh the correctly named process is already added later
+        if self.replace_with_sm125:
+            self.PROC_SETS.append("ggh")
         # Open input root file to read the histograms
         rf = ROOT.TFile(self.filename, "read")
 
         for X in procs:
-            if "H" == X: # don't create histogram for 'H' in cases, where its mass is a model-parameter
-                continue
-            self.doHistFunc("m_{}".format(X), rf.Get("m_{}".format(X)), pars) # syntax: Higgs-Boson, mass attribute, parameters
-
-        for X in procs:
+            if self.x_variable.replace("m", "") == X:  # don't create histogram for 'H' in cases, where its mass is a model-parameter
+                pass
+            else:
+                self.doHistFunc("m{}".format(X), rf.Get("m_{}".format(X)), pars)  # syntax: Higgs-Boson, mass attribute, parameters
             self.doHistFunc("br_{}tautau".format(X), rf.Get("br_{}tautau".format(X)), pars) # syntax: Higgs-Boson, xsec attribute, parameters, production mode
 
             self.doHistFunc("xs_gg{}".format(X), rf.Get("xs_gg{}".format(X)), pars) # syntax: Higgs-Boson, xsec attribute, parameters, production mode
             self.doHistFunc("xs_bb{}".format(X), rf.Get("xs_bb{}".format(X)), pars) # syntax: Higgs-Boson, xsec attribute, parameters, production mode
 
-            # self.doHistFuncFromXsecTools(X, "yukawa_top", pars)
-            # self.doHistFuncFromXsecTools(X, "yukawa_bottom", pars)
             for loopcontrib in ['t', 'b']:
-                # self.doHistFuncForYukawas(X, loopcontrib, pars)
                 self.doHistFunc("Y{}_MSSM_{}".format(loopcontrib, X), rf.Get("g{}_{}".format(loopcontrib, X)), pars)
 
-            self.add_ggH_at_NLO('xs_gg{X}{LC}', X)  # TODO: Maybe reenable ggH pT reweighting at NLO
+            self.add_ggH_at_NLO('xs_gg{X}{LC}', X)
 
             # ggH scale uncertainty
             self.doAsymPowSystematic(X, "xs", pars, "gg", "scale")
-            # # ggH pdf+alpha_s uncertainty
-            # self.doAsymPowSystematic(X, "xsec", pars, "gg", "pdfas")
-            # # bbH total uncertainty
-            # self.doAsymPowSystematic(X, "xsec", pars, "bb", "total")
+            # ggH pdf+alpha_s uncertainty
+            self.doAsymPowSystematic(X, "xs", pars, "gg", "pdfas")
+            # bbH total uncertainty in MSSM?
+            # bbH pdf+alpha_s uncertainty
+            self.doAsymPowSystematic(X, "xs", pars, "bb", "pdfas")
 
             for loopcontrib in ['t','b','i']:
                 self.SYST_DICT['xs_gg%s_%s' % (X, loopcontrib)].append('systeff_xs_gg%s_2HDM_scale' %X)
+                self.SYST_DICT['xs_gg%s_%s' % (X, loopcontrib)].append('systeff_xs_gg%s_2HDM_pdfas' %X)
+
+            self.SYST_DICT['xs_bb%s' %X].append('systeff_xs_bb%s_2HDM_pdfas' %X)
 
             # Make a note of what we've built, will be used to create scaling expressions later
             self.PROC_SETS.append('bb%s'%X)
-            self.PROC_SETS.extend(['gg%s_t'%X, 'gg%s_b'%X, 'gg%s_i'%X])
+            if X != "h":
+                self.PROC_SETS.append('bb%s_lowmass'%X)
+            self.PROC_SETS.extend(['gg%s_t'%X, 'gg%s_b'%X, 'gg%s_i'%X, 'gg%s_t_lowmass'%X, 'gg%s_b_lowmass'%X, 'gg%s_i_lowmass'%X])
 
+        # Add BSM prediction for qqPhi processes
+        self.doHistFuncForQQH(pars)
+        self.PROC_SETS.extend(["qqh", "Wh", "Zh"])
         # And the SM terms
         self.PROC_SETS.extend(['ggH125', 'qqH125', 'ZH125', 'WH125', 'bbH125'])
         if self.debug_output:
@@ -136,24 +185,19 @@ class THDMvsSMHiggsModel(PhysicsModel):
         """Create POI and other parameters, and define the POI set."""
         self.modelBuilder.doVar("r[1,0,20]")
 
-        # MSSMvsSM
+        # THDMvsSM
         self.modelBuilder.doVar("x[1,0,1]")
         self.modelBuilder.out.var('x').setConstant(True)
         self.modelBuilder.factory_("expr::not_x(\"(1-@0)\", x)")
         self.sigNorms = { True:'x', False:'not_x' }
 
-        # Create variable to set processes to zero
-        self.modelBuilder.doVar('z[0,0,1]')
-        self.modelBuilder.out.var('z').setConstant(True)
-
         self.modelBuilder.doSet('POI', 'r') # TODO: Check what this does and if it is correct
 
         # We don't intend on actually floating these in any fits...
-        self.modelBuilder.out.var(self.x_variable).setConstant(True) # either mA or mHp
+        self.modelBuilder.out.var(self.x_variable).setConstant(True) # either mH or cos(beta-alpha)
         self.modelBuilder.out.var('tanb').setConstant(True)
 
-        # bsm_proc_match = "(gg(A|H|h)_(t|i|b)|bb(A|H|h))"
-        bsm_proc_match = "(gg(A|H|h)_(t|b|i)|bb(A|H|h))"
+        bsm_proc_match = "(gg(A|H|h)_(t|b|i)|bb(A|H|h))(|_lowmass)"
         # Go over set of defined signal processes and scale the processes with the
         # correct normalizations.
         for proc in self.PROC_SETS:
@@ -161,20 +205,34 @@ class THDMvsSMHiggsModel(PhysicsModel):
             terms = []
             # Get Higgs boson type from process name
             X = proc.split('_')[0].replace('gg','').replace('bb','')
-            # For standard modell processes add flag to be able to deactivate them
+            # For standard model processes add flag to be able to deactivate them
             if "H125" in proc:
                 terms = [self.sigNorms[False]]
             # In case of BSM signals scale the process with cross section and BR from
             # model prediction, free parameter r and flag it as BSM signal
             elif re.match(bsm_proc_match, proc): # not SM-like BSMSCALAR: either h or H
                 terms.extend([
-                    'xs_%s' % proc, # %proc.rstrip("_t"),  # TODO: Temp. fix for using only top contribution
+                    'xs_%s' % proc.replace("_lowmass", ""),
                     'br_%stautau'%X,
                     'r',
                     self.sigNorms[True],
                     ])
-            else:
-                terms = ["z"]
+            elif re.match("(qqh|Wh|Zh)$", proc):
+                terms.extend([
+                    'sf_qqphi_2HDM',
+                    'r',
+                    self.sigNorms[True],
+                    ])
+            elif re.match("ggh$", proc):
+                terms.extend([
+                    'xs_%s' % proc.replace("_lowmass", ""),
+                    'br_%stautau'%X,
+                    'r',
+                    self.sigNorms[True],
+                    '%s' % str(1./float(self.sm_predictions["xs_gg_SMH125"])),  # Divide by SM values as input template is already scaled
+                    '%s' % str(1./float(self.sm_predictions["br_SMH125_tautau"]))  # to SM prediction of xs and br
+                    ])
+
             print(terms)
 
             # Check if a term was added that is associated with
@@ -187,8 +245,15 @@ class THDMvsSMHiggsModel(PhysicsModel):
             terms.extend(extra)
             print(proc, terms)
             # Add scaling function for the process to the workspace
-            print('prod::scaling_%s(%s)'%(proc,','.join(terms)))
-            self.modelBuilder.factory_('prod::scaling_%s(%s)'%(proc,','.join(terms)))
+            if re.match("ggh$", proc) and self.replace_with_sm125 and self.use_hSM_difference:
+                self.modelBuilder.factory_('prod::bsm_scaling_%s(%s)'%(proc,','.join(terms))) # Add scaling of BSM process: mu*SF = x*r*SF
+                self.modelBuilder.factory_('expr::scaling_%s(\"(@0 - @1 * @2)\", %s)'%(proc,','.join(["bsm_scaling_%s"%proc,"x","r"]))) # Add difference between BSM prediction and mu*SM prediction.
+            elif re.match("bbh$", proc) and self.use_hSM_difference:
+                self.modelBuilder.factory_('prod::bsm_scaling_%s(%s)'%(proc,','.join(terms)))
+                self.modelBuilder.factory_('expr::scaling_%s(\"(@0 - @1 * @2 * %s * %s)\", %s)'%(proc,str(self.sm_predictions["xs_bb_SMH125"]),str(self.sm_predictions["br_SMH125_tautau"]),','.join(["bsm_scaling_%s"%proc,"x","r"])))
+            else:
+                print('prod::scaling_%s(%s)'%(proc,','.join(terms)))
+                self.modelBuilder.factory_('prod::scaling_%s(%s)'%(proc,','.join(terms)))
             self.modelBuilder.out.function('scaling_%s'%proc).Print('')
 
     def doAsymPowSystematic(self, higgs, quantity, varlist, production, uncertainty):
@@ -258,15 +323,9 @@ class THDMvsSMHiggsModel(PhysicsModel):
         This is not needed if high-mass categorisation is used as there is no sensitivity to
         the qqh process in this categorisation.
         """
-        # TODO: Is this description correct?
-        raise NotImplementedError
-
-    def doHistFuncForYukawas(self, X, contrib, varlist, thdm_type2=True):
-        """Calculate Yukawa coupling from known value of cos(beta-alpha) and tanb value in grid.
-
-        Temporary solution until the yukawa couplings will be available in the histograms.
-        """
-        name = "Y{c}_MSSM_{h}".format(c=contrib, h=X)
+        # Set up histogram to be translated to HistFunc object.
+        name = "sf_qqphi_2HDM"
+        # TODO: Only temporary necessary until correct histograms are saved.
         x_parname = varlist[0].GetName()
         x_binning = self._binning[os.path.basename(self.filename).replace(".root", "")][x_parname]
 
@@ -276,37 +335,23 @@ class THDMvsSMHiggsModel(PhysicsModel):
         # TODO: name needs to be fixed
         y_step = (y_binning[1]-y_binning[0])/2.
         hist = ROOT.TH2D(name, name, len(x_binning)-1, x_binning, len(y_binning)-1, y_binning)
+        # Open input root file to read the histograms
+        rf = ROOT.TFile(self.filename, "read")
+        # In the latest version of the inputs sin(b-a) and cos(b-a) are
+        # already contained and can therefore be read directly.
+        scale_sin = rf.Get("sin(beta-alpha)")
         for i_x, x in enumerate(x_binning):
             for i_y, y in enumerate(y_binning):
-                beta = math.atan(hist.GetYaxis().GetBinCenter(i_y+1))
-                alpha = beta - math.acos(0.01) # cos(beta-alpha) = 0.01
-
-                if X == "h":
-                    if contrib == "t":
-                        value = math.cos(alpha) / math.sin(beta)
-                    elif contrib == "b":
-                        value = - math.sin(alpha) / math.cos(beta) if thdm_type2 else math.cos(alpha) / math.sin(beta)
-                    else:
-                        raise RuntimeError("Given contribution '{}' not known.".format(contrib))
-                elif X == "H":
-                    if contrib == "t":
-                        value = math.sin(alpha) / math.sin(beta)
-                    elif contrib == "b":
-                        value = math.cos(alpha) / math.cos(beta) if thdm_type2 else math.sin(alpha) / math.sin(beta)
-                    else:
-                        raise RuntimeError("Given contribution '{}' not known.".format(contrib))
-                elif X == "A":
-                    if contrib == "t":
-                        value = - 1. / math.tan(beta) # scaling as -cot(beta)
-                    elif contrib == "b":
-                        value = - math.tan(beta) if thdm_type2 else 1. / math.tan(beta)
-                    else:
-                        raise RuntimeError("Given contribution '{}' not known.".format(contrib))
-                else:
-                    raise RuntimeError("Given higgs boson '{}' not knwon.".format(X))
-
+                # Want to scale qqh process with h SM like Higgs boson by sin(beta-alpha)**2
+                # beta can be obtained from the binning on the y axis
+                # beta = math.atan(hist.GetYaxis().GetBinCenter(i_y+1))
+                # alpha = math.asin(gt_H.GetBinContent(i_x+1, i_y+1)*math.sin(beta))
+                # value = math.sin(beta - alpha)**2
+                value = scale_sin.GetBinContent(i_x+1, i_y+1)**2
+                # Subtract SM contribution when mu only scales the difference
+                if self.use_hSM_difference:
+                    value -= 1.0
                 hist.SetBinContent(i_x+1, i_y+1, value)
-
         return self.doHistFunc(name, hist, varlist)
 
 
--- run_model-dep-thdm_limits.sh
+++ run_model-dep-thdm_limits.sh
@@ -7,6 +7,7 @@ MODE=$2
 MODEL=$3
 ANALYSISTYPE=$4
 
+GRIDUSER=$(whoami)
 analysis="bsm-model-dep-additional"
 sm_like_hists="sm125"
 replace_with_sm125=1
@@ -23,7 +24,7 @@ else
 fi
 
 case "$MODEL" in
-    "THDM_BP1_TYPE1")
+    "THDM_BP1_Type1")
         wsoutput="ws_thdm_bp1_type1.root"
         modelfile="BP1_Type1.root"
         # scenario_label="M_{h}^{125} scenario (H,A#rightarrow#tau#tau)"
@@ -34,7 +35,18 @@ case "$MODEL" in
         # y_min=1.0
         # y_max=60.0
         ;;
-    "THDM_BP1_TYPE2")
+    "THDM_BP1_Type1_mod")
+        wsoutput="ws_thdm_bp1_type1_mod.root"
+        modelfile="BP1_Type1_mod.root"
+        # scenario_label="M_{h}^{125} scenario (H,A#rightarrow#tau#tau)"
+        sub_analysis="sm-like-light"
+        # sm_like_mass="m_h"
+        x_title='m_{H} [GeV]'
+        # mass_histogram_title="m_{h}"
+        # y_min=1.0
+        # y_max=60.0
+        ;;
+    "THDM_BP1_Type2")
         wsoutput="ws_thdm_bp1_type2.root"
         modelfile="BP1_Type2.root"
         # scenario_label="M_{h}^{125} scenario (H,A#rightarrow#tau#tau)"
@@ -45,7 +57,7 @@ case "$MODEL" in
         # y_min=1.0
         # y_max=60.0
         ;;
-    "THDM_FixedMass_TYPE2")
+    "THDM_FixedMass_Type2")
         wsoutput="ws_thdm_fixedmass_type2.root"
         modelfile="FixedMass_Type2.root"
         # scenario_label="M_{h}^{125} scenario (H,A#rightarrow#tau#tau)"
@@ -56,12 +68,56 @@ case "$MODEL" in
         # y_min=1.0
         # y_max=60.0
         ;;
+    "THDM_PASComp_Type1")
+        wsoutput="ws_thdm_pascomp_type1.root"
+        modelfile="PASComp_Type1.root"
+        # scenario_label="M_{h}^{125} scenario (H,A#rightarrow#tau#tau)"
+        sub_analysis="sm-like-light"
+        # sm_like_mass="m_h"
+        x_title='m_{H} [GeV]'
+        # mass_histogram_title="m_{h}"
+        # y_min=1.0
+        # y_max=60.0
+        ;;
+    "THDM_PASComp_Type2")
+        wsoutput="ws_thdm_pascomp_type2.root"
+        modelfile="PASComp_Type2.root"
+        # scenario_label="M_{h}^{125} scenario (H,A#rightarrow#tau#tau)"
+        sub_analysis="sm-like-light"
+        # sm_like_mass="m_h"
+        x_title='m_{H} [GeV]'
+        # mass_histogram_title="m_{h}"
+        # y_min=1.0
+        # y_max=60.0
+        ;;
+    "THDM_HWWLike_Type1")
+        wsoutput="ws_thdm_hwwlike_type1.root"
+        modelfile="HWWLike_Type1.root"
+        # scenario_label="M_{h}^{125} scenario (H,A#rightarrow#tau#tau)"
+        sub_analysis="sm-like-light"
+        # sm_like_mass="m_h"
+        x_title='m_{H} [GeV]'
+        # mass_histogram_title="m_{h}"
+        # y_min=1.0
+        # y_max=60.0
+        ;;
+    "THDM_HWWLike_Type2")
+        wsoutput="ws_thdm_hwwlike_type2.root"
+        modelfile="HWWLike_Type2.root"
+        # scenario_label="M_{h}^{125} scenario (H,A#rightarrow#tau#tau)"
+        sub_analysis="sm-like-light"
+        # sm_like_mass="m_h"
+        x_title='m_{H} [GeV]'
+        # mass_histogram_title="m_{h}"
+        # y_min=1.0
+        # y_max=60.0
+        ;;
     *)
-        echo "[ERROR] Given model $MODEL not known..."
+        echo -e "\033[0;31m[ERROR]\033[0m Given model $MODEL not known..."
         exit 1
 esac
 
-defaultdir="analysis_v14/$TAG"
+defaultdir="analysis_2022_02_28/$TAG"
 [[ ! -d ${defaultdir} ]] && mkdir -p ${defaultdir}
 defaultdir=$(readlink -f ${defaultdir})
 [[ ! -d ${defaultdir} ]] && mkdir -p ${defaultdir}
@@ -84,12 +140,12 @@ if [[ $MODE == "initial" ]]; then
             --categorization ${categorization} \
             --sm-like-hists ${sm_like_hists} \
             --sm-gg-fractions ${CMSSW_BASE}/src/CombineHarvester/MSSMvsSMRun2Legacy/data/higgs_pt_reweighting_fullRun2.root \
-            --additional-arguments "--auto_rebin=1 --real_data=0 --manual_rebin=1" \
+            --additional-arguments "--auto_rebin=1 --real_data=1 --manual_rebin=1" \
             --eras 2016,2017,2018 \
             --category-list ${CMSSW_BASE}/src/CombineHarvester/MSSMvsSMRun2Legacy/input/mssm_classic_categories.txt \
             --variable mt_tot_puppi \
             --parallel 10 2>&1 | tee -a ${defaultdir}/logs/morph_mssm_log.txt
-    else
+    elif [[ $ANALYSISTYPE == "with-sm-ml" ]]; then
         morph_parallel.py --output ${defaultdir}/datacards \
             --analysis ${analysis} \
             --sub-analysis ${sub_analysis} \
@@ -97,7 +153,7 @@ if [[ $MODE == "initial" ]]; then
             --categorization ${categorization} \
             --sm-like-hists ${sm_like_hists} \
             --sm-gg-fractions ${CMSSW_BASE}/src/CombineHarvester/MSSMvsSMRun2Legacy/data/higgs_pt_reweighting_fullRun2.root \
-            --additional-arguments "--auto_rebin=1 --real_data=0 --manual_rebin=1 --split_sm_signal_cat=1" \
+            --additional-arguments "--auto_rebin=1 --real_data=1 --manual_rebin=1 --split_sm_signal_cat=1" \
             --eras 2016,2017,2018 \
             --category-list ${CMSSW_BASE}/src/CombineHarvester/MSSMvsSMRun2Legacy/input/sm_neuralnet_categories.txt \
             --variable nnscore \
@@ -111,11 +167,13 @@ if [[ $MODE == "initial" ]]; then
             --categorization ${categorization} \
             --sm-like-hists ${sm_like_hists} \
             --sm-gg-fractions ${CMSSW_BASE}/src/CombineHarvester/MSSMvsSMRun2Legacy/data/higgs_pt_reweighting_fullRun2.root \
-            --additional-arguments "--auto_rebin=1 --real_data=0 --manual_rebin=1" \
+            --additional-arguments "--auto_rebin=1 --real_data=1 --manual_rebin=1" \
             --eras 2016,2017,2018 \
             --category-list ${CMSSW_BASE}/src/CombineHarvester/MSSMvsSMRun2Legacy/input/mssm_new_categories.txt \
             --variable mt_tot_puppi \
             --parallel 10 2>&1 | tee -a ${defaultdir}/logs/morph_mssm_log.txt
+    else
+        echo -e "\033[0;31m[ERROR]\033[0m Given analysis type `${ANALYSISTYPE}` not known. Please try again..."
     fi
 
     ############
@@ -134,6 +192,16 @@ if [[ $MODE == "initial" ]]; then
             rsync -av --progress ${datacarddir}/${era}/htt_${channel}*/* ${datacarddir}/${era}/${channel}/ 2>&1 | tee -a ${defaultdir}/logs/copy_datacards_${era}_${channel}.txt
         done
     done
+    # Check if the expected number of datacards has been written
+    if [[ $ANALYSISTYPE == "classic" ]]; then
+        EXPECTED=$(((4+4+2+7)*3))
+    else
+        EXPECTED=$(((15+15+11+18)*3))
+    fi
+    if [[ $(ls ${datacarddir}/combined/cmb/*.txt | wc -l) != $EXPECTED ]]; then
+        echo -e "\033[0;31m[ERROR]\033[0m Not all datacards have been created or written. Please check the logs..."
+        echo "Expected ${EXPECTED} datacards written but found only $(ls ${datacarddir}/combined/cmb/*.txt | wc -l) in the combined directory."
+    fi
 
 elif [[ $MODE == "ws" ]]; then
     ############
@@ -144,7 +212,11 @@ elif [[ $MODE == "ws" ]]; then
     --PO filePrefix=${CMSSW_BASE}/src/CombineHarvester/MSSMvsSMRun2Legacy/data/ \
     --PO modelFile=${modelfile} \
     --PO MSSM-NLO-Workspace=${CMSSW_BASE}/src/CombineHarvester/MSSMvsSMRun2Legacy/data/higgs_pt_reweighting_fullRun2.root \
-    -i ${datacarddir}/2016/mt/ 2>&1 | tee -a ${defaultdir}/logs/workspace_${MODEL}.txt
+    --PO replace-with-sm125=${replace_with_sm125} \
+    --PO sm-predictions=${CMSSW_BASE}/src/CombineHarvester/MSSMvsSMRun2Legacy/input/sm_predictions_13TeV.json \
+    -i ${datacarddir}/combined/cmb/ 2>&1 | tee -a ${defaultdir}/logs/workspace_${MODEL}.txt
+    # Not needed as only relevant for full limits including h signal hypothesis
+    # --PO hSM_treatment="hSM-in-bg" \
 
 elif [[ "$MODE" == "setup" ]]; then
     ############
@@ -153,7 +225,7 @@ elif [[ "$MODE" == "setup" ]]; then
     cd ${defaultdir}/limits_${MODEL}/condor
     combineTool.py -M AsymptoticGrid \
     ${CMSSW_BASE}/src/CombineHarvester/MSSMvsSMRun2Legacy/input/thdm_asymptotic_grid_${MODEL}.json \
-    -d ${datacarddir}/2016/mt/${wsoutput} \
+    -d ${datacarddir}/combined/cmb/${wsoutput} \
     --job-mode 'condor' \
     --task-name $taskname \
     --dry-run \
@@ -161,11 +233,11 @@ elif [[ "$MODE" == "setup" ]]; then
     --setParameterRanges r=0,1 \
     --setParameters r=1,x=1 \
     --freezeParameters x -v 1 \
-    -t -1 \
     --cminDefaultMinimizerStrategy 0 \
     --X-rtd MINIMIZER_analytic \
     --merge 5 \
     --cminDefaultMinimizerTolerance 0.01 2>&1 | tee -a ${defaultdir}/logs/job_setup_${MODEL}.txt
+    # -t -1 \
 
 elif [[ $MODE == "submit" ]]; then
     ############
@@ -182,6 +254,27 @@ elif [[ $MODE == "submit-local" ]]; then
     cd ${defaultdir}/limits_${MODEL}/condor
     python run_limits_locally.py --cores 20 --taskname condor_${taskname}.sh
 
+elif [[ $MODE == "submit-gc" ]]; then
+    ############
+    # job submission
+    ############
+    gcworkdir=${defaultdir}/limits_${MODEL}/gc_condor
+    mkdir -p ${gcworkdir}
+    python scripts/build_gc_job.py \
+        --combine-script ${defaultdir}/limits_${MODEL}/condor/condor_${taskname}.sh \
+        --workspace ${datacarddir}/combined/cmb/${wsoutput} \
+        --workdir ${gcworkdir} \
+        --tag ${taskname} \
+        --se-path /storage/gridka-nrg/${GRIDUSER}/gc_storage/combine/${taskname}
+
+    ${CMSSW_BASE}/src/grid-control/go.py ${gcworkdir}/${taskname}.conf -Gc -m 3
+
+elif [[ $MODE == "copy-results-gc" ]]; then
+    ############
+    # job submission
+    ############
+    rsync -avhP /storage/gridka-nrg/${GRIDUSER}/gc_storage/combine/${taskname}/output/ ${defaultdir}/limits_${MODEL}/condor
+
 elif [[ $MODE == "collect" ]]; then
     ############
     # job collection
@@ -189,18 +282,18 @@ elif [[ $MODE == "collect" ]]; then
     cd ${defaultdir}/limits_${MODEL}/condor
     combineTool.py -M AsymptoticGrid \
     ${CMSSW_BASE}/src/CombineHarvester/MSSMvsSMRun2Legacy/input/thdm_asymptotic_grid_${MODEL}.json \
-    -d ${datacarddir}/2016/mt/${wsoutput} \
+    -d ${datacarddir}/combined/cmb/${wsoutput} \
     --job-mode 'condor' \
     --task-name $taskname2 \
     --dry-run \
-    --redefineSignalPOI x \
-    --setParameterRanges x=0,1 \
-    --setParameters r=1 \
-    --freezeParameters r -v 1 \
-    -t -1 \
+    --redefineSignalPOI r \
+    --setParameterRanges r=0,1 \
+    --setParameters x=1,r=1 \
+    --freezeParameters x -v 1 \
     --cminDefaultMinimizerStrategy 0 \
     --X-rtd MINIMIZER_analytic \
     --cminDefaultMinimizerTolerance 0.01 2>&1 | tee -a ${defaultdir}/logs/collect_jobs_${MODEL}.txt
+    # -t -1 \
 
     # # condor_submit condor_${taskname2}.sub
     cp asymptotic_grid.root ..
@@ -210,18 +303,22 @@ elif [[ $MODE == "collect" ]]; then
     # limit plot
     ############
     if [[ $ANALYSISTYPE == "classic" ]]; then
-        title="Classic categorisation 36 fb^{-1} (2016, 13 TeV)"
+        titleleft='--title-left="Classic_categorisation"'
+        title="#font[62]{CMS} data 138 fb^{-1} (13 TeV)"
     else
-        title="138 fb^{-1} (13 TeV)"
+        titleleft=""
+        title="#font[62]{CMS} data 138 fb^{-1} (13 TeV)"
     fi
-    plotLimitGrid.py asymptotic_grid.root \
+    echo $titleleft
+    ${CMSSW_BASE}/src/CombineHarvester/CombineTools/scripts/plotLimitGrid.py asymptotic_grid.root \
     --scenario-label="${scenario_label}" \
-    --output ${TAG}_${MODEL} \
+    --output ${TAG}_${MODEL}_obs \
     --title-right="${title}" \
-    --cms-sub="Preliminary" \
-    --contours="exp-2,exp-1,exp0,exp+1,exp+2" \
+    ${titleleft} \
+    --contours="exp-2,exp-1,exp0,exp+1,exp+2,obs" \
     --thdm-validity --logy \
     --model_file=${CMSSW_BASE}/src/CombineHarvester/MSSMvsSMRun2Legacy/data/${modelfile} \
+    --y-title "tan#kern[0.16667]{#beta}" \
     --x-title "${x_title}" 2>&1 | tee -a ${defaultdir}/logs/plot_grid_${MODEL}.txt
     # --y-range ${y_min},${y_max} \
     # --mass_histogram ${sm_like_mass} \
